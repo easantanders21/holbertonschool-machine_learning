0df868013c72dff57fac9f0cbd8efd136e8541ea
easantanders21 <3823@holbertonschool.com>
2022-07-28 12:37:13 -0500

    task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""calculates the transpose of a matrix"""
+
+
+def np_transpose(matrix):
+    """ function that calculates the transpose of a matrix"""
+    return matrix.transpose()
