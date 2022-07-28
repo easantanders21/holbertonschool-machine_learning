4acf86e038080f806a59421410e170ccf2b12a57
easantanders21 <3823@holbertonschool.com>
2022-07-27 21:01:47 -0500

    task 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Function that concatenates two arrays:"""
+    new_arr = []
+    new_arr = arr1 + arr2
+    return(new_arr)
