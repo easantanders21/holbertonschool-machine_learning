2cc18cdd77cd1768299e28a538b6143498e0fd29
easantanders21 <3823@holbertonschool.com>
2022-07-28 17:51:38 -0500

    task 15

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Slices a matrix along specific axes"""
+
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """Slices a matrix along specific axes"""
+    slices = []
+    axes={0: (2,), 2: (None, None, -2)}
+    for i in range(matrix.ndim):
+        t = axes.get(i)
+        if t is not None:
+            slices.append(slice(*t))
+        else:
+            slices.append(slice(None, None, None))
+    
+    return matrix[tuple(slices)]
