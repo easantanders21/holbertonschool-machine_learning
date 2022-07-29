f833a2a80ced9cd76c02ab2268c8073ecebdfeb2
easantanders21 <3823@holbertonschool.com>
2022-07-28 20:00:59 -0500

    task 16

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """function that adds two matrices"""
-import numpy as np
 
 
 def add_matrices(mat1, mat2):
     """function that adds two matrices"""
+    import numpy as np
     add_matrix = []
     mat1, mat2 = np.array(mat1), np.array(mat2)
     if not mat1.shape == mat2.shape:
