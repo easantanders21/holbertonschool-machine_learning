a68aedc173c14c421ecfd13d407cbfb9101dc747
easantanders21 <3823@holbertonschool.com>
2022-08-10 17:35:20 -0500

    task 0-1-2

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,46 +1,57 @@
 #!/usr/bin/env python3
-"""  class Poisson """
-e = 2.7182818285
+"""Poisson class module"""
 
 
 class Poisson:
-    """  class Poisson that represents a poisson distribution """
+    """Poisson probability distribution class"""
+
+    e = 2.7182818285
+
+    @staticmethod
+    def factorial(n):
+        """
+        Factorial
+        """
+        fact = 1
+        for num in range(2, n + 1):
+            fact *= num
+        return fact
 
     def __init__(self, data=None, lambtha=1.):
-        """ class Poisson constructor """
+        """Poisson object attributes initialization"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
-        elif type(data) is not list:
-            raise TypeError("data must be a list")
-        elif len(data) <= 1:
-            raise ValueError("data must contain multiple values")
         else:
-            self.lambtha = float(sum(data)/len(data))
-
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                for value in data:
+                    if type(value) not in [float, int]:
+                        raise ValueError("data must contain multiple values")
+                self.lambtha = sum(data)/len(data)
 
     def pmf(self, k):
-        """ instance method pmf """
-        if k is not int:
-            try:
-                k = int(k)
-            except:
-                return 0
+        """Calculates the value of the Poisson PMF"""
+        if type(k) is not int:
+            k = int(k)
         if k < 0:
             return 0
-        pmf = (e ** (-self.lambtha) * self.lambtha ** (k)) / factorial(k)
-        return pmf
-
+        else:
+            return (((self.lambtha**k)*(Poisson.e**(-self.lambtha))) /
+                    Poisson.factorial(k))
 
     def cdf(self, k):
-        """ instance method cdf """
-        pass
-        
-
-def factorial(num):
-    """ calculates a factorial """
-    factorial = 1
-    for i in range(1,num + 1):    
-       factorial = factorial * i
-    return factorial
+        """Calculates the value of the Poisson CDF"""
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        else:
+            k_sumattion = 0
+            for m in range(0, k+1):
+                k_sumattion += (self.lambtha**m)/Poisson.factorial(m)
+            return (Poisson.e**(-self.lambtha))*k_sumattion
