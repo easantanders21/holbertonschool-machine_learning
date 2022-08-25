2959ffb3d74ad8c4597be4108f3b67a90ea0db80
easantanders21 <3823@holbertonschool.com>
2022-08-25 18:56:11 -0500

    task 2 fix

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ forward propagation """
-import tensorflow.compat.v1 as tf
+create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """ forward propagation """
-    create_layer = __import__('1-create_layer').create_layer
     for i in range(len(layer_sizes)):
-        layer = create_layer(x, layer_sizes[i], activations[i])
-        x = layer
-    return
+        if i == 0:
+            pre = create_layer(x, layer_sizes[i], activations[i])
+        else:
+            pre = create_layer(prev, layer_sizes[i], activations[i])
+    return pre
