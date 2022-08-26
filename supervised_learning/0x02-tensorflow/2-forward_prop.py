2209bfac13ae6b9531e6e4ae9b9987cc5fb9e49d
easantanders21 <3823@holbertonschool.com>
2022-08-25 19:04:13 -0500

    task 2 fix

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """ forward propagation """
-create_layer = __import__('1-create_layer').create_layer
 
 
-def forward_prop(x, layer_sizes=[], activations=[]):
+def forward_prop(x, layer_sizes=[], activations=[], index=0):
     """ forward propagation """
-    for i in range(len(layer_sizes)):
-        if i == 0:
-            pre = create_layer(x, layer_sizes[i], activations[i])
-        else:
-            pre = create_layer(prev, layer_sizes[i], activations[i])
-    return pre
+    if index >= len(layer_sizes):
+        return x
+    create_layer = __import__('1-create_layer').create_layer
+    layer = create_layer(x, layer_sizes[index], activations[index])
+    return forward_prop(layer, layer_sizes, activations, index + 1)
