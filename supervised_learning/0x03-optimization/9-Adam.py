d9c5e350ddc4dbe6a8dcb4a4f58149514071672e
easantanders21 <3823@holbertonschool.com>
2022-09-01 15:11:33 -0500

    task 9

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -7,6 +7,6 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     v = beta1 * v + (1 - beta1) * grad
     s = beta2 * s + (1 - beta2) * np.square(grad)
     v_corrected = v / (1 - beta1 ** t)
-    s_corrected = v / (1 - beta2 ** t)
+    s_corrected = s / (1 - beta2 ** t)
     var -= alpha * v_corrected / (epsilon + np.sqrt(s_corrected))
     return var, v, s
