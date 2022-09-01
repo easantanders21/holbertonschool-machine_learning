8f0f63bbf6d2d45d70d7b92f7ca77e0baab6dcb2
easantanders21 <3823@holbertonschool.com>
2022-09-01 16:11:42 -0500

    task 14

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
 """ Batch Normalization Upgraded """
+
 import tensorflow.compat.v1 as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
     """ Batch Normalization Upgraded """
-    initializer = tf.\
-        contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 
-    base_layer = tf.layers.Dense(units=n, kernel_initializer=initializer,
-                                 name="base_layer")
-    X = base_layer(prev)
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(units=n, kernel_initializer=initializer)
+    mean, variance = tf.nn.moments(layer(prev), axes=[0])
+    gamma = tf.ones([n])
+    beta = tf.zeros([n])
 
-    mean, variance = tf.nn.moments(X, axes=[0])
-    gamma = tf.Variable(tf.constant(1.0, shape=[n]), trainable=True,
-                        name="gamma")
-    beta = tf.Variable(tf.constant(0.0, shape=[n]), trainable=True,
-                       name="beta")
     epsilon = 1e-8
-    Z = tf.nn.batch_normalization(x=X, mean=mean, variance=variance,
-                                  offset=beta, scale=gamma,
-                                  variance_epsilon=epsilon, name="Z")
-    A = activation(Z)
-    return A
+    batch_normalization_output = tf.nn.batch_normalization(
+        x=layer(prev), mean=mean,
+        variance=variance, offset=beta,
+        scale=gamma, variance_epsilon=epsilon)
+
+    return activation(batch_normalization_output)
