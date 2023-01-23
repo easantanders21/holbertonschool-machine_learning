465ebac8e3cc4518734d8b2a2d77e3c518cddf50
easantanders21 <3823@holbertonschool.com>
2023-01-22 19:13:36 -0500

    task 11

---
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""function positional_encoding"""
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """
+    Function that calculates the positional encoding for a transformer.
+    Arguments:
+        max_seq_len: int
+            the maximum sequence length
+        dm: dimension of embedding vector
+            the model depth
+    Returns:
+        PE: numpy.ndarray of shape (max_seq_len, dm)
+            positional encoding vectors
+    """
+    PE = np.zeros([max_seq_len, dm])
+    for pos in range(max_seq_len):
+        for i in range(0, dm, 2):
+            PE[pos, i] = np.sin(pos / (10000 ** (2 * (i // 2) / dm)))
+            PE[pos, i + 1] = np.cos(pos / (10000 ** (2 * (i // 2) / dm)))
+    return PE
---
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Scaled Dot Product Attention"""
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """
+    Function that calculates the scaled dot product attention.
+    https://www.tensorflow.org/tutorials/text/transformer#scaled_dot_product_attention
+    Arguments:
+        Q: tensor with its last two dimensions as (..., seq_len_q, dk)
+            the query matrix
+        K: tensor with its last two dimensions as (..., seq_len_v, dk)
+            the key matrix
+        V: tensor with its last two dimensions as (..., seq_len_v, dv)
+            the value matrix
+        mask: tensor that can be broadcast into (..., seq_len_q, seq_len_v)
+            the optional mask, defaulted to None
+    Returns:
+        output, weights
+        output: tensor with its last two dimensions as (..., seq_len_q, dv)
+            the scaled dot product attention
+        weights: tensor with its last two dimensions as (..., seq_len_q,
+                                                         seq_len_v)
+            the attention weights
+    """
+    QKT = tf.matmul(Q, K, transpose_b=True)
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scaled_attention_logits = QKT / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+    output = tf.matmul(attention_weights, V)
+
+    return output, attention_weights
