3c08967fb7c6bd589ea44d2d9489a342f5f61f12
easantanders21 <3823@holbertonschool.com>
2023-07-20 11:16:03 -0500

    fix task 4

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 """
-3-upcoming.py
+3. What will be next?
 """
-from requests import get
+import requests
+
 
 if __name__ == '__main__':
-    url = 'https://api.spacexdata.com/v4/launches/upcoming'
-    response = get(url)
-    data = response.json()
-    launch = data[0]
-    name = launch.get('name')
-    date = launch.get('date_local')
-    rocket = launch.get('rocket')
-    launchpad = launch.get('launchpad')
-    print('{} ({}) {} - {} ({})'.format(name, date, rocket, launchpad))
+    url = "https://api.spacexdata.com/v4/launches/upcoming"
+    response = requests.get(url)
+    json = response.json()
+
+    dates = [x['date_unix'] for x in json]
+    index = dates.index(min(dates))
+    next_launch = json[index]
+
+    name = next_launch['name']
+    date = next_launch['date_local']
+    rocket = next_launch['rocket']
+    launchpad = next_launch['launchpad']
+
+    url_rocket = "https://api.spacexdata.com/v4/rockets/" + rocket
+    req_rocket = requests.get(url_rocket)
+    json_rocket = req_rocket.json()
+    rocket_name = json_rocket['name']
+
+    url_launchpad = "https://api.spacexdata.com/v4/launchpads/" + launchpad
+    req_launchpad = requests.get(url_launchpad)
+    json_launchpad = req_launchpad.json()
+    launchpad_name = json_launchpad['name']
+    launchpad_loc = json_launchpad['locality']
+
+    info = (name + ' (' + date + ') ' + rocket_name + ' - ' +
+            launchpad_name + ' (' + launchpad_loc + ')')
+    print(info)
--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 """
-4-rocket_frequency.py
+4. How many by rocket?
 """
-from requests import get
+from datetime import datetime
+import requests
+
 
 if __name__ == '__main__':
     url = 'https://api.spacexdata.com/v4/launches'
-    response = get(url)
-    data = response.json()
-    rockets = {}
-    for launch in data:
+    response = requests.get(url).json()
+    rocketDict = {}
+    for launch in response:
         rocket = launch.get('rocket')
-        if rocket in rockets:
-            rockets[rocket] += 1
+        url = 'https://api.spacexdata.com/v4/rockets/{}'.format(rocket)
+        response = requests.get(url).json()
+        rocket = response.get('name')
+        if rocketDict.get(rocket) is None:
+            rocketDict[rocket] = 1
         else:
-            rockets[rocket] = 1
-    for rocket, launches in sorted(rockets.items(), key=lambda x: (-x[1], x[0])):
-        print('{}: {}'.format(rocket, launches))
+            rocketDict[rocket] += 1
+    rocketList = sorted(rocketDict.items(), key=lambda kv: kv[0])
+    rocketList = sorted(rocketList, key=lambda kv: kv[1], reverse=True)
+    for rocket in rocketList:
+        print("{}: {}".format(rocket[0], rocket[1]))
