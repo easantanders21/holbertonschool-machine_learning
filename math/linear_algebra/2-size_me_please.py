50daccf2b829e76fc2e446e9d1628c684754fca4
easantanders21 <3823@holbertonschool.com>
2024-07-31 15:38:11 -0500

    fix code style

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Shadow for size the len of matrix"""
 
 def matrix_shape(matrix):
+    """Function that calculates the shape of a matrix:"""
     shape = []
     while type(matrix) is list:
         shape.append(len(matrix))
--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Transpose of a 2D matrix"""
 
 def matrix_transpose(matrix):
+    """function that returns the transpose of a 2D matrix"""
     matrix_t = []
     for i in range(len(matrix[0])):
         new_row = []
