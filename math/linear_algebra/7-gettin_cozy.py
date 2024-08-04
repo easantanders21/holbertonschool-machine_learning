d9e0054a6cd3b5cc2c22a5af7368343942942529
easantanders21 <3823@holbertonschool.com>
2024-08-04 09:48:53 -0500

    task 7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+''' concatenates two matrices along a specific axis '''
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """function that concatenates two matrices along a specific axis"""
+    new_mat = []
+    if axis == 0:
+        new_mat = [*mat1, *mat2]
+        return new_mat
+    elif axis != 0:
+        new_mat = list(range(len(mat1)))
+        for i in range(len(mat1)):
+            new_mat[i] = cat_matrices2D(mat1[i], mat2[i], axis - 1)
+        return new_mat
+    else:
+        return None
