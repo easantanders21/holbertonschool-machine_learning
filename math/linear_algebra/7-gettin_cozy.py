94e07aa779ce7d255461fab82cd4cf9b3ec2dc95
easantanders21 <3823@holbertonschool.com>
2024-08-04 10:16:31 -0500

    task 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,25 +4,14 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Function that concatenates two matrices along a specific axis"""
-    if not isinstance(mat1, list) or not isinstance(mat2, list):
-        return None
+    new_mat = []
     if axis == 0:
-        # Check if both matrices have the same number of columns
-        if all(len(row) == len(mat1[0]) for row in mat1) and all(len(row) == len(mat2[0]) for row in mat2):
-            return [*mat1, *mat2]
-        else:
-            return None
-    elif axis == 1:
-        # Check if both matrices have the same number of rows
-        if len(mat1) == len(mat2):
-            new_mat = []
-            for row1, row2 in zip(mat1, mat2):
-                if len(row1) == len(row2):
-                    new_mat.append(row1 + row2)
-                else:
-                    return None
-            return new_mat
-        else:
-            return None
+        new_mat = [*mat1, *mat2]
+        return new_mat
+    elif axis != 0:
+        new_mat = list(range(len(mat1)))
+        for i in range(len(mat1)):
+            new_mat[i] = cat_matrices2D(mat1[i], mat2[i], axis - 1)
+        return new_mat
     else:
         return None
