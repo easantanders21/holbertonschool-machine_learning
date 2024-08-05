afc5a907b5e91837d123a84b7fb3e6d95a05a78f
easantanders21 <3823@holbertonschool.com>
2024-08-04 20:17:42 -0500

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Write a function that performs matrix multiplication '''
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    ''' function that performs matrix multiplication '''
+    return np.dot(mat1, mat2)
