b0eb696397a0c72f30392f7682af2b6803ee9d88
easantanders21 <3823@holbertonschool.com>
2024-08-04 19:40:27 -0500

    task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''  calculates the shape of a numpy.ndarray '''
+
+
+def np_shape(matrix):
+    """ function that calculates the shape of a array"""
+    return matrix.shape
