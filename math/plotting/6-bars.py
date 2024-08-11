9d77a62b9365e859f7c4d153019b335d2431cd60
easantanders21 <3823@holbertonschool.com>
2024-08-11 18:13:30 -0500

    fix pycodestyle task 6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+''' bar graph '''
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def bars():
+    ''' bar graph '''
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
     columns = ['Farrah', 'Fred', 'Felicia']
     apples = fruit[0]
@@ -15,11 +18,11 @@ def bars():
     fig, ax = plt.subplots()
     ax.bar(columns, apples, width, label='apples', color='red')
     ax.bar(columns, bananas, width, bottom=fruit[0],
-        label='bananas', color='yellow')
+           label='bananas', color='yellow')
     ax.bar(columns, oranges, width, bottom=fruit[1] + fruit[0],
-        label='oranges', color='#ff8000')
+           label='oranges', color='#ff8000')
     ax.bar(columns, peaches, width, bottom=fruit[2] + fruit[1] + fruit[0],
-        label='peaches', color='#ffe5b4')
+           label='peaches', color='#ffe5b4')
     ax.set_ylabel('Quantity of Fruit')
     ax.set_ylim([0, 80])
     ax.set_title('Number of Fruit per Person')
